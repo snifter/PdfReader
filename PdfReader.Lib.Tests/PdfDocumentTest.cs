1adb0c6dab59b427f990567765e00a46ba2b7ca7
snifter <marekpodsiadly@gmail.com>
2020-07-01 22:10:32 +0200

    pages count test

---
+++ PdfReader.Lib.Tests/PdfDocumentTest.cs
@@ -0,0 +1,16 @@
+﻿using Xunit;
+
+namespace PdfReader.Lib.Tests
+{
+    public class PdfDocumentTest
+    {
+        [Fact]
+        public void PagesCount_ReturnsValidCount()
+        {
+            const string filePath = "../testFiles/LoremIpsum.pdf";
+            var target = new PdfDocument(filePath);
+            
+            Assert.Equal(10, target.PagesCount);
+        }
+    }
+}
---
+++ PdfReader.Lib/PdfDocument.cs
@@ -0,0 +1,12 @@
+﻿namespace PdfReader.Lib
+{
+    public class PdfDocument
+    {
+        public int PagesCount { get; }
+        
+        public PdfDocument(string filePath)
+        {
+            
+        }
+    }
+}
